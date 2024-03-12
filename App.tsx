ec6d329437c81042ed8b25317adfbeb2346adcca
nabeelelahi <nabeelelahi2000@gmail.com>
2024-03-12 23:36:54 +0500

    feat: react-native project initiated

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>
+          Oshopi
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
